f3168076774608a8a5572253d85413b6e1fb91a4
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-21 08:38:48 +0530

    Update

---
+++ client/src/pages/HomePage/Classgymdata.jsx
@@ -0,0 +1,47 @@
+import class1img from '../../assets/class-1.jpg'
+import class2img from '../../assets/class-2.jpg'
+import class3img from '../../assets/class-3.jpg'
+import class4img from '../../assets/class-4.jpg'
+import class5img from '../../assets/class-5.jpg'
+
+const gymdata = [
+    {
+        id: 1,
+        name: 'STRENGTH',
+        link: '#',
+        title: 'Weightlifting',
+        img: class1img,
+    },
+
+    {
+        id: 2,
+        name: 'Cardio',
+        link: '#',
+        title: 'Indoor cycling',
+        img: class2img,
+    },
+
+    {
+        id: 3,
+        name: 'STRENGTH',
+        link: '#',
+        title: 'Kettlebell power',
+        img: class3img,
+    },
+
+    {
+        id: 4,
+        name: 'Cardio',
+        link: '#',
+        title: 'Indoor cycling',
+        img: class4img,
+    },
+
+    {
+        id: 5,
+        name: 'Training',
+        link: '#',
+        title: 'Boxing',
+        img: class5img,
+    },
+]
