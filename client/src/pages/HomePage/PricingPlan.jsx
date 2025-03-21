7c51e8363992597da20becd0f9a419ada6645b31
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-21 10:20:29 +0530

    Update PricingPlan.jsx

--- client/src/pages/HomePage/PricingPlan.jsx
+++ client/src/pages/HomePage/PricingPlan.jsx
@@ -2,26 +2,31 @@ import React from 'react'
 import { gymplan } from './PlanData'
 
 const PricingPlan = () => {
-  return (
-    <div className='text-white text-center bg-[#151515] py-24 xl:px-40 md:px-20 px-4'>
-        <h1 className="text-orange-500 uppercase text-2xl">Our Plan</h1>
-        <h1 className="text-3xl uppercase">Choose your pricing plan</h1>
+    return (
+        <div className='text-white text-center bg-[#151515] py-24 xl:px-40 md:px-20 px-4'>
+            <h1 className="text-orange-500 uppercase text-2xl">Our Plan</h1>
+            <h1 className="text-3xl uppercase">Choose your pricing plan</h1>
 
-        <div className="">
-            <div className="grid md:grid-cols-3 gap-6 mt-6">
-                {
-                    gymplan.map((data, index) => {
-                        return (
-                            <div className="bg-transparent border border-orange-500 p-8" key={index}>
-                                
-                            </div>
-                        )
-                    })
-                }
+            <div className="">
+                <div className="grid md:grid-cols-3 gap-6 mt-6">
+                    {
+                        gymplan.map((data, index) => {
+                            return (
+                                <div className="duration-500 hover:bg-white bg-transparent border border-orange-500 hover:border-white p-8 group" key={index}>
+                                    <center>
+                                        <div className="duration-500 h-auto rounded-full w-28 p-8 group-hover:bg-orange-500">
+                                            <data.icon className='h-12 w-auto' />
+                                        </div>
+                                    </center>
+                                </div>
+
+                            )
+                        })
+                    }
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default PricingPlan
