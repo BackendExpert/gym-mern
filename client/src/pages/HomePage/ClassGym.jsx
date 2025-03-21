0b178528bcdf3e0750d6ef0b733901c7f0a68e58
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-21 09:32:12 +0530

    Update

--- client/src/pages/HomePage/ClassGym.jsx
+++ client/src/pages/HomePage/ClassGym.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { gymdata } from './Classgymdata'
+import bodyb from '../../assets/bodybuilder.png'
+import { FaPhone } from "react-icons/fa";
 
 const ClassGym = () => {
     return (
@@ -49,7 +51,27 @@ const ClassGym = () => {
                 }
 
                 <div className="pl-4 xl:w-1/2 xl:mt-0 mt-8 bg-[#0a0a0a] xl:ml-4 rounded p-8 ml-0">
-                    
+                    <div className="flex">
+                        <div className="md:w-2/3 md:mt-24">
+                            <h1 className="text-3xl md:text-left pl-8">TRANSFORM YOUR BODY & MIND WITH <span className='text-orange-500'>FITNESS WORKSOUTS !!</span></h1>
+
+                            <div className="">
+                                <div className="bg-orange-500 text-white p-6 md:mr-16 md:ml-8 ml-4 mt-6 rounded-full">
+                                    <div className="flex">
+                                        <FaPhone className='h-10 w-auto'/>
+                                        <div className="ml-4">
+                                            <h1 className="">FOR MORE INFO..</h1>
+                                            <p className="md:text-2xl font-semibold">+94 75 462 3575</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="w-1/3 flex justify-end mt-16 md:block hidden">
+                            <img src={bodyb} alt="" className='h-80 w-auto' />
+                        </div>
+                    </div>
                 </div>
             </div>
 
