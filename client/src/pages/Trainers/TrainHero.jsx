64917fa1fffdd6f5a4b5574b24bafc7256a14061
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-22 19:53:16 +0530

    Update TrainHero.jsx

--- client/src/pages/Trainers/TrainHero.jsx
+++ client/src/pages/Trainers/TrainHero.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const TrainHero = () => {
-  return (
-    <div>TrainHero</div>
-  )
+    return (
+        <div className='relative bg-[url(https://wallpapercave.com/wp/wp10391824.jpg)] bg-cover bg-center min-h-[60vh]'>
+            <div className='absolute inset-0 bg-black opacity-70'></div>
+            <div className="relative text-white pt-64 xl:px-28 md:px-40 px-4 text-center">
+                <h1 className="text-4xl">Trainers | <span className='text-orange-500'>FitFlex</span></h1>
+                <p className="">Login to System for Hire a Trainer</p>
+            </div>
+        </div>
+    )
 }
 
 export default TrainHero
