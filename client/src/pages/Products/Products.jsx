e60c3079837fc71d52ea0040aa1f40c186dcee94
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-22 18:45:01 +0530

    Update

---
+++ client/src/pages/Products/Products.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProductHero from './ProductHero'
+
+const Products = () => {
+  return (
+    <div>
+        <ProductHero />
+    </div>
+  )
+}
+
+export default Products
