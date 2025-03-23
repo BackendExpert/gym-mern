9cc574a4f68697483c34ed3b7f83a1e7fb9ac9de
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-23 17:48:37 +0530

    Update

--- server/models/UserActivity.js
+++ server/models/UserActivity.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const UserActivitySchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
+    email: { type: String, required: true },
     activity: { type: String, required: true }
 }, {timestamps: true});
 
