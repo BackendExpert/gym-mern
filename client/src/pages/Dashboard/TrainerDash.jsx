6456c63c19dec31e7293c5b8dba7439308c650fb
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-24 09:05:42 +0530

    Updte

--- client/src/pages/Dashboard/TrainerDash.jsx
+++ client/src/pages/Dashboard/TrainerDash.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
 import { BiSolidDashboard } from 'react-icons/bi'
 import GymStats from '../../components/OtherCom/GymStats'
+import { FaUsers } from 'react-icons/fa'
+import { FaFileLines, FaDumbbell, FaUser } from 'react-icons/fa6'
+import CountUp from 'react-countup'
 
 const TrainerDash = () => {
+    const trainercard = [
+        {
+            id: 1,
+            name: 'Total Members',
+            value: 500,
+            icon: FaUsers            ,
+            title: 'For Me',
+        },
+        {
+            id: 2,
+            name: 'Total Workout Plans',
+            value: 500,
+            icon: FaFileLines,
+            title: 'in FitFlex',
+        },
+        {
+            id: 3,
+            name: 'Weekly Scheduled Sessions',
+            value: 500,
+            icon: FaDumbbell,
+            title: 'for Me',
+        },
+    ]
     return (
         <div className='mt-4 text-white mr-4'>
             <div className="flex">
@@ -14,6 +40,34 @@ const TrainerDash = () => {
                 <div className="pl-4">
                     <h1 className="text-orange-500 text-xl pt-1 font-semibold uppercase">Dashboard</h1>
                 </div>
+            </div>
+
+            <div className="grid xl:grid-cols-4 md:grid-cols-2 gap-4 mt-4">
+                <div className="flex bg-orange-500 p-8 rounded-lg">
+                    <div className="">
+                        <FaUser className='h-10 w-auto'/>
+                    </div>
+                    <div className="ml-4">
+                        <h1 className="">Jehan</h1>
+                        <p className="uppercase text-xl font-semibold">trainer</p>
+                    </div>
+                </div>
+                {
+                    trainercard.map((data, index) => {
+                        return (
+                            <div className=" rounded-lg flex" key={index}>
+                                <div className="bg-orange-500 p-6 pt-6 rounded-l-lg">
+                                    <data.icon className='h-10 w-auto'/>
+                                    <p className="pt-4 text-xl font-semibold"><CountUp end={data.value} duration={4}/> </p>
+                                </div>
+                                <div className="bg-[#151515] p-6 w-full rounded-r-lg border-b-2 border-orange-500">
+                                    <h1 className="text-orange-500 font-semibold uppercase">{data.name}</h1>
+                                    <p className="pt-4">{data.title}</p>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
             </div>
 
 
